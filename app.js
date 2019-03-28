b1cb9eb15a73d2275ee63e1c4a02d8fad8cdb3aa
leo <18883285722@163.com>
2019-03-28 16:12:47 +0800

    fix: 启动start修改

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ module.exports = app => {
     fs.writeFileSync(path.join(__dirname, 'pid.cache'), process.pid);
   });
   app.ready(() => {
-    if (!app.config.startAfterInit) return;
+    if (!app.config.grpcServer.startAfterInit) return;
     const pid = fs.readFileSync(path.join(__dirname, 'pid.cache'), { encoding: 'utf8' });
     if (Number(pid) === process.pid) {
       const grpcServer = new app.GrpcServer(app);
